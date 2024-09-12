79cc21f5b1b606c8de58b24520826958a58878ca
Priscila Caballero <priscila.g.caballero@gmail.com>
2024-09-12 14:48:01 -0500

    OPCION 7 LISTAS

--- app/src/main/java/com/example/demo_ui_exa_1/fragment_option7.kt
+++ app/src/main/java/com/example/demo_ui_exa_1/fragment_option7.kt
@@ -1,11 +1,15 @@
 package com.example.demo_ui_exa_1
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import java.util.*
+
 class FragmentOption7 : Fragment() {
 
     override fun onCreateView(
@@ -15,8 +19,31 @@ class FragmentOption7 : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_option7, container, false)
 
+        val datePicker = view.findViewById<DatePicker>(R.id.datePicker)
+        val calculateButton = view.findViewById<Button>(R.id.btn_calcularedad)
+        val ageTextView = view.findViewById<TextView>(R.id.txt_edad)
+
+        calculateButton.setOnClickListener {
+            val day = datePicker.dayOfMonth
+            val month = datePicker.month
+            val year = datePicker.year
+
+            val birthdayCalendar = Calendar.getInstance()
+            birthdayCalendar.set(year, month, day)
+
+            val today = Calendar.getInstance()
+
+            var age = today.get(Calendar.YEAR) - birthdayCalendar.get(Calendar.YEAR)
+
+            // Si aún no ha cumplido años este año, resta 1
+            if (today.get(Calendar.DAY_OF_YEAR) < birthdayCalendar.get(Calendar.DAY_OF_YEAR)) {
+                age--
+            }
+
+            // Mostrar la edad calculada en el TextView
+            ageTextView.text = "Edad: $age años"
+        }
 
         return view
     }
-
 }
